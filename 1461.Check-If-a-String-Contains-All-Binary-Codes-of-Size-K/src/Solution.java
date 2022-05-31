724632fb3a199828a6e219f5f56a76f29688d800
Abhyuday Ghatale <30296052+Abhyu08@users.noreply.github.com>
2022-05-31 09:24:59 +0530

    Add files via upload

---
+++ 1461.Check-If-a-String-Contains-All-Binary-Codes-of-Size-K/src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Solution {
+    public static boolean hasAllCodes(String s, int k) {
+        int need = 1 << k;
+        Set<String> got = new HashSet<String>();
+
+        for (int i = k; i <= s.length(); i++) {
+            String a = s.substring(i - k, i);
+            if (!got.contains(a)) {
+                got.add(a);
+                need--;
+                // return true when found all occurrences
+                if (need == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int k = sc.nextInt();
+        System.out.println(hasAllCodes(str, k));
+    }
+}
+/*
+00110110
+2
+
+0110
+1
+
+0110
+2
+
+
+ */
